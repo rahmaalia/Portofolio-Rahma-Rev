41897d99158ed13e4e6f3cf44e04472de94c2c49
rahmaalia <rahmaaliaputri27@gmail.com>
2022-01-07 14:42:16 +0700

    finish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul'); 
+const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
+  nav.classList.toggle('slide');
 });
 
 //javascript for navigation bar effect on scroll
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("nav");
-    header.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('nav');
+  header.classList.toggle('sticky', window.scrollY > 0);
 });
 
+//li active
 var selector = '.nav li';
 
-$(selector).on('click', function(){
-    $(selector).removeClass('active');
-    $(this).addClass('active');
+$(selector).on('click', function () {
+  $(selector).removeClass('active');
+  $(this).addClass('active');
 });
